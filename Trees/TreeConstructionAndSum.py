609da947d6c16886093fcb9b1027198b7bbe5f76
Amit Nijsure <amitnijsure1234@gmail.com>
2020-05-31 10:44:31 +0530

    Tree Traversals - Inorder, Preorder, Postorder

--- Trees/TreeConstructionAndSum.py
+++ Trees/TreeConstructionAndSum.py
@@ -1,5 +1,14 @@
-from .tree import Tree
+class Tree:
+    
+    def __init__(self, cargo, left=None, right=None):
+        self.cargo = cargo
+        self.left = left
+        self.right = right
+    
+    def __str__(self):
+        return str(self.cargo)
 
+traversal_array = []
 
 def sum_tree_elements(my_tree):
     if my_tree == None:
@@ -7,7 +16,40 @@ def sum_tree_elements(my_tree):
     return sum_tree_elements(my_tree.left) + sum_tree_elements(my_tree.right) + my_tree.cargo
 
 
-my_tree = Tree(3, Tree(2, Tree(1), Tree(4)), Tree(5, Tree(6), Tree(7)))
-sum_result = sum_tree_elements(my_tree)
-print("\nTree Element Addition Result : ", str(sum_result), "\n")
+def inorder_traversal(root):
+    if root:
+        inorder_traversal(root.left)
+        traversal_array.append(root.cargo)
+        inorder_traversal(root.right)
 
+
+def preorder_traversal(root):
+    if root:
+        traversal_array.append(root.cargo)
+        preorder_traversal(root.left)
+        preorder_traversal(root.right)
+
+
+def postorder_traversal(root):
+    if root:
+        postorder_traversal(root.left)
+        postorder_traversal(root.right)
+        traversal_array.append(root.cargo)
+
+
+my_tree = Tree(1, Tree(2, Tree(4), Tree(5)), Tree(3, Tree(6), Tree(7)))
+sum_result = sum_tree_elements(my_tree)
+print("\nTree Element Addition Result : ", str(sum_result))
+print("\n*****TreeTraversal*****")
+print("1. Inorder Traversal")
+inorder_traversal(my_tree)
+print(traversal_array)
+traversal_array = []
+print("\n2. Preorder Traversal")
+preorder_traversal(my_tree)
+print(traversal_array)
+traversal_array = []
+print("\n3. Postorder Traversal")
+postorder_traversal(my_tree)
+print(traversal_array)
+traversal_array = []
